bd39392e7dfb31797913957602788c8c4ab169fc
Maria Anne San Andres <mariannetsa1799@gmail.com>
2025-08-17 01:23:04 +0800

    fix: responsive height for mobile and desktop

--- src/components/Badges.jsx
+++ src/components/Badges.jsx
@@ -3,11 +3,14 @@ import badges from "../data/badges.json";
 export default function Badges() {
   return (
     <div className="py-10 flex flex-col justify-center items-center space-y-6 h-auto my-10">
-      <div
-        className={`flex justify-center gap-5 sm:gap-6 md:gap-12 md:h-20 h-16 flex-wrap`}
-      >
+      <div className={`flex justify-center gap-5 sm:gap-6 md:gap-12`}>
         {badges.map((badge) => (
-          <img src={badge.img} alt={badge.badge} key={badge.id} />
+          <img
+            src={badge.img}
+            alt={badge.badge}
+            key={badge.id}
+            className="md:h-20 h-16"
+          />
         ))}
       </div>
     </div>
