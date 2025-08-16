8d06d92fab35bee8167a92ddce4ee36c6e784017
Maria Anne San Andres <mariannetsa1799@gmail.com>
2025-08-17 01:17:37 +0800

    feat: added flex-wrap for badge section

--- src/components/Badges.jsx
+++ src/components/Badges.jsx
@@ -4,7 +4,7 @@ export default function Badges() {
   return (
     <div className="py-10 flex flex-col justify-center items-center space-y-6 h-auto my-10">
       <div
-        className={`flex justify-center gap-5 sm:gap-6 md:gap-12 md:h-20 h-16`}
+        className={`flex justify-center gap-5 sm:gap-6 md:gap-12 md:h-20 h-16 flex-wrap`}
       >
         {badges.map((badge) => (
           <img src={badge.img} alt={badge.badge} key={badge.id} />
